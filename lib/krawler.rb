9e62b28e9d2c2fef57f3fb6b3b319b988b5512b2
Mike Evans <mike@urlgonomics.com>
2012-05-15 09:47:35 -0400

    Added support to restrict crawl to a path

--- lib/krawler.rb
+++ lib/krawler.rb
@@ -1,24 +1,29 @@
 require 'krawler/version'
 require 'mechanize'
 require 'timeout'
+require 'uri'
+require 'pry'
 
 module Krawler
 
   class Base
 
     def initialize(url, options)
-      @base = url
-      @agent = Mechanize.new
-      @links_to_crawl = [@base]
+      url = URI(url)
+      @host           = "#{url.scheme}://#{url.host}"
+      @base_path      = url.path
+      @agent          = Mechanize.new
+      @links_to_crawl = [url]
       @crawled_links  = []
       @bad_links      = []
       @suspect_links  = []
       @exclude        = options[:exclude]
+      @restrict       = options[:restrict]
     end
   
     def base
-      puts "Crawling #{@base}"
-    
+      puts "Crawling..."
+
       while !@links_to_crawl.empty? do
         crawl_page(@links_to_crawl.pop)
       end
@@ -51,11 +56,19 @@ module Krawler
   
       return if !page.respond_to?(:links)
       page.links.each do |new_link|
-        new_link = new_link.href
-        if (new_link =~ /^#{Regexp.escape(@base)}/) || (new_link =~ /^\//)
-          next if @crawled_links.include?(new_link)
-          next if @exclude && new_link =~ /#{@exclude}/
-    
+        begin
+          new_url = URI(new_link.href)
+          new_link = new_url.to_s
+        rescue ArgumentError # junk link
+          next
+        end
+
+        if (new_link =~ /^#{Regexp.escape(@host)}/) || (new_link =~ /^\//) # don't crawl external domains
+
+          next if @crawled_links.include?(new_link)       # don't crawl what we've alread crawled
+          next if @exclude && new_link =~ /#{@exclude}/   # don't crawl excluded matched paths
+          next if @restrict && (new_url.path !~ /^#{Regexp.escape(@base_path)}/) # don't crawl outside of our restricted base path
+          
           @links_to_crawl << new_link
         end
       end
