93315047d59a8099fbf6d8da780615751176a234
Joe.Winter <joe.winter@sungard.com>
2013-02-26 12:29:48 -0500

    Apply the restrict and include rules to recaching invalid content, so that we don't recache invalid content unless we want to

--- lib/krawler.rb
+++ lib/krawler.rb
@@ -112,6 +112,7 @@ module Krawler
       @mutex.synchronize do
         return if !page.respond_to?(:links)
 
+        # This doesn't follow any of the rules below...
         recache_invalid_results(page)
 
         page.links.each do |new_link|
@@ -168,7 +169,11 @@ module Krawler
         query = params_to_hash(uri.query || '')
         query['cache'] = 'false'
         uri.query = hash_to_params(query)
-        @links_to_crawl << uri.to_s
+        if @restrict  # don't crawl outside of our restricted base path
+          if @include && uri.path =~ /#{@include}/ # unless we match our inclusion
+            @links_to_crawl << uri.to_s
+          end
+        end
       end
     end
   end
